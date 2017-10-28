ee6e76a43bca5b4af27a29c821acd62fa0b6cbfa
takuyamakawa <g2117052@fun.ac.jp>
2017-10-29 01:16:12 +0900

    画面上部に影追加

--- client/emil/emil/Coupon.swift
+++ client/emil/emil/Coupon.swift
@@ -11,16 +11,20 @@ import UIKit
 class Coupon: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var Coupon: UITableView!
+    @IBOutlet weak var shadow: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         Coupon.register(UINib(nibName: "CustomCellTableViewCell", bundle: nil), forCellReuseIdentifier: "CustomCellTableViewCell")
-
-        // Delegate設定
-        Coupon.delegate = self
         
-        // DataSource設定
+        
+        shadow.layer.shadowColor = UIColor.black.cgColor
+        shadow.layer.shadowOpacity = 0.3 // 透明度
+        shadow.layer.shadowOffset = CGSize(width: 5, height: 5) // 距離
+        shadow.layer.shadowRadius = 5 // ぼかし量
+        
+        Coupon.delegate = self
         Coupon.dataSource = self
     }
     
@@ -31,7 +35,11 @@ class Coupon: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // セルを作る
-        
+        /*
+         * To Do
+         * 画像を乗っける
+         * ギフトを配列に変える
+         */
         let cell = Coupon.dequeueReusableCell(withIdentifier: "CustomCellTableViewCell", for: indexPath) as! CustomCellTableViewCell
         cell.gift.text = "ハンバーーーーグ"
         cell.smileage.text = String(120)

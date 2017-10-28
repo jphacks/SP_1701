381a0ed9ea04fdc09d48a634ac5fcd902a52ca24
takuyamakawa <g2117052@fun.ac.jp>
2017-10-28 12:09:26 +0900

    時間割配置

--- client/emil/emil/TimeTable.swift
+++ client/emil/emil/TimeTable.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
+extension UIColor {
+    class func lightBlue() -> UIColor {
+        return UIColor(red: 92.0 / 255, green: 192.0 / 255, blue: 210.0 / 255, alpha: 1.0)
+    }
+    
+    class func lightRed() -> UIColor {
+        return UIColor(red: 195.0 / 255, green: 123.0 / 255, blue: 175.0 / 255, alpha: 1.0)
+    }
+}
+
 class TimeTable: UIViewController {
+    
+    @IBOutlet weak var total_smile: UILabel!
+    @IBOutlet weak var oneday_smile: UILabel!
+    
+    @IBOutlet weak var timetabelColelctionView: UICollectionView!
+    
+    let weekArray = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        timetabelColelctionView.delegate = self as? UICollectionViewDelegate
+        timetabelColelctionView.dataSource = self as? UICollectionViewDataSource
+        timetabelColelctionView.backgroundColor = UIColor.green
     }
     
     override func didReceiveMemoryWarning() {

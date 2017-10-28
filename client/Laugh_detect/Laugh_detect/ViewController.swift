d1da67ce7df03c66c37300eefb5fba46358a5d43
YutoKumagai <b1013130@fun.ac.jp>
2017-10-28 16:51:12 +0900

    ボタンを押したら録音する処理を追加

--- client/Laugh_detect/Laugh_detect/ViewController.swift
+++ client/Laugh_detect/Laugh_detect/ViewController.swift
@@ -7,19 +7,85 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var recordButton: UIButton!
+    
+    var audioRecorder: AVAudioRecorder?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        /// 録音可能カテゴリに設定する
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        } catch  {
+            // エラー処理
+            fatalError("カテゴリ設定失敗")
+        }
+        
+        // sessionのアクティブ化
+        do {
+            try session.setActive(true)
+        } catch {
+            // audio session有効化失敗時の処理
+            // (ここではエラーとして停止している）
+            fatalError("session有効化失敗")
+        }
+        
+        self.setupAudioRecorder()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func setupAudioRecorder() {
+        
+        // 録音用URLを設定
+        let dirURL = documentsDirectoryURL()
+        print(dirURL)
+        let fileName = "recording.caf"
+        let recordingsURL = dirURL.appendingPathComponent(fileName)
+        
+        // 録音設定
+        let recordSettings: [String: AnyObject] =
+            [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue as AnyObject,
+             AVEncoderBitRateKey: 16 as AnyObject,
+             AVNumberOfChannelsKey: 2 as AnyObject,
+             AVSampleRateKey: 44100.0 as AnyObject]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: recordingsURL!, settings: recordSettings)
+        } catch {
+            audioRecorder = nil
+        }
+        
+    }
+    
+    /// DocumentsのURLを取得
+    func documentsDirectoryURL() -> NSURL {
+        let urls = FileManager.default.urls(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask)
+        
+        if urls.isEmpty {
+            
+            fatalError("URLs for directory are empty.")
+        }
+        
+        return urls[0] as NSURL
+    }
+    
+    @IBAction func startRecordButtonTapped(_ sender: Any) {
+        if (audioRecorder?.isRecording)! {
+            audioRecorder?.stop()
+        }else{
+            audioRecorder?.record()
+        }
+    }
+    
 }
 

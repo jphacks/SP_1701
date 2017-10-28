9bde56f26865c62f5f6a879d6516bda24c92dc29
takuyamakawa <g2117052@fun.ac.jp>
2017-10-28 16:16:01 +0900

    曜日を表示

--- client/emil/emil/TimeTable.swift
+++ client/emil/emil/TimeTable.swift
@@ -18,26 +18,82 @@ extension UIColor {
     }
 }
 
-class TimeTable: UIViewController {
+class TimeTable: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
     
     @IBOutlet weak var total_smile: UILabel!
     @IBOutlet weak var oneday_smile: UILabel!
     
     @IBOutlet weak var timetabelColelctionView: UICollectionView!
     
-    let weekArray = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-    
+    let weekArray = ["日", "月", "火", "水", "木", "金", "土"]
+    let cellMargin: CGFloat = 2.0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        timetabelColelctionView.delegate = self as? UICollectionViewDelegate
-        timetabelColelctionView.dataSource = self as? UICollectionViewDataSource
-        timetabelColelctionView.backgroundColor = UIColor.green
+        timetabelColelctionView.delegate = self
+        timetabelColelctionView.dataSource = self
+        timetabelColelctionView.backgroundColor = UIColor.white
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //1
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    //2
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // Section毎にCellの総数を変える.
+        if section == 0 {
+            return 7
+        } else {
+            return 70
+        }
+    }
+    //3
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath as IndexPath) as! CalendarCell
+        
+        //テキストカラー
+        if (indexPath.row % 7 == 0) {
+            cell.textLabel.textColor = UIColor.red
+        } else if (indexPath.row % 7 == 6) {
+            cell.textLabel.textColor = UIColor.blue
+        } else {
+            cell.textLabel.textColor = UIColor.gray
+        }
+        //テキスト配置
+        if indexPath.section == 0 {
+            cell.textLabel.text = weekArray[indexPath.row]
+        } else {
+            cell.textLabel.text = "あ"
+            //月によって1日の場所は異なる(後ほど説明します)
+        }
+        
+        return cell
+    }
+    
+    //セルのサイズを設定
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfMargin: CGFloat = 8.0
+        let width: CGFloat = (collectionView.frame.size.width - cellMargin * numberOfMargin) / CGFloat(7)
+        let height: CGFloat = width * 1.0
+        return CGSize(width:width, height:height)
+        
+    }
+    
+    //セルの垂直方向のマージンを設定
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return cellMargin
+    }
+    
+    //セルの水平方向のマージンを設定
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return cellMargin
     }
     
     @IBAction func backToMain(_ sender: Any) {

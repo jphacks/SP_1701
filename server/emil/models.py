545d6ea85bfbeb38512a91f5263f0c4f8690bfff
Kenta Iwami <g2117009@fun.ac.jp>
2017-10-28 16:27:17 +0900

    modelの定義

--- server/emil/models.py
+++ server/emil/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import string
+import random
 
-# Create your models here.
+
+def gen_rand_str():
+    length = 10
+    chars = string.digits + string.ascii_letters
+    return ''.join([random.choice(chars) for _ in range(length)])
+
+
+class User(models.Model):
+    random_id = models.CharField(max_length=10, default=gen_rand_str, unique=True, blank=False)
+    smileage = models.IntegerField(default=0)
+
+
+class Laughs(models.Model):
+    user = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
